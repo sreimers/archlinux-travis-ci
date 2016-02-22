0bc41309dd63176f1798e196cdea591e1da7b69f
Sebastian Reimers <info@miete-admin.de>
2016-02-22 18:11:32 +0100

    try arch-travis

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget https://mirrors.kernel.org/archlinux/iso/2016.01.01/archlinux-bootstrap-2016.01.01-x86_64.tar.gz
-tar -xzf archlinux-bootstrap-2016.01.01-x86_64.tar.gz
-
-sudo root.x86_64/bin/arch-chroot root.x86_64
-pacman -Syu
---
+++ build_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+
+echo "Hello World"
+id
+ls -lha
+env
