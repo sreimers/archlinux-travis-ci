149f22d70ea799a434171509d96064b007754136
Sebastian Reimers <info@miete-admin.de>
2016-02-22 17:42:50 +0100

    add sudo

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 wget https://mirrors.kernel.org/archlinux/iso/2016.01.01/archlinux-bootstrap-2016.01.01-x86_64.tar.gz
 tar -xzf archlinux-bootstrap-2016.01.01-x86_64.tar.gz
 
-root.x86_64/bin/arch-chroot root.x86_64
+sudo root.x86_64/bin/arch-chroot root.x86_64
 pacman -Syu
